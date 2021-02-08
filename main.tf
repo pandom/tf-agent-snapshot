a7137d4ac057069e1ce1cf30641a7fd21c5895fc
pandom <pandom@users.noreply.github.com>
2021-02-08 16:05:28 +1100

    Remove Jobs

--- main.tf
+++ main.tf
@@ -1,7 +1,48 @@
 provider "nomad" {
     address = var.nomad_address
 }
+#Backend Nomad Configuration
+resource "nomad_scheduler_config" "config" {
+  scheduler_algorithm = "spread"
+  preemption_config = {
+    system_scheduler_enabled  = true
+    batch_scheduler_enabled   = true
+    service_scheduler_enabled = true
+  }
+}
 
-resource "nomad_job" "app" {
-  jobspec = file("${path.module}/jobs/redist.nomad")
+// resource "nomad_job" "app" {
+//   jobspec = file("${path.module}/jobs/redist.nomad")
+// }
+
+resource "nomad_namespace" "dev" {
+  name        = "dev"
+  description = "Shared development environment."
+  quota       = "dev"
+}
+
+resource "nomad_namespace" "production" {
+  name        = "production"
+  description = "Shared development environment."
+  quota       = "production"
+}
+
+resource "nomad_namespace" "playground" {
+  name        = "playground"
+  description = "Shared development environment."
+  quota       = "playground"
+}
+
+resource "nomad_quota_specification" "prod_api" {
+  name        = "prod-api"
+  description = "Production instances of backend API servers"
+
+  limits {
+    region = "DC1"
+
+    region_limit {
+      cpu       = 2400
+      memory_mb = 1200
+    }
+  }
 }
