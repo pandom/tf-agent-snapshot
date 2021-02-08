95f7608101224d22c5a1d6f60b01ab423920b9b8
pandom <pandom@users.noreply.github.com>
2021-02-08 15:51:31 +1100

    redist.nomad

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "nomad" {
     address = var.nomad_address
 }
 
-resource "nomad_job" "snapshot_app" {
-    jobspec = file(./jobs/redist.nomad)
+resource "nomad_job" "app" {
+  jobspec = file("${path.module}/jobs/redist.nomad")
 }
